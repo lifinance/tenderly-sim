6c016146368223295390c13be35eed66a45335b3
myz1237 <mengyizhuoprivate@outlook.com>
2025-03-03 19:01:09 +0800

    feat(chains): add apechain to tenderly config

--- src/tenderly/tenderly.config.ts
+++ src/tenderly/tenderly.config.ts
@@ -53,6 +53,7 @@ export const TENDERLY_CHAINS: number[] = [
   ChainId.LSK, // Lisk - 1135
   ChainId.SON, // Sonic - 146
   ChainId.UNI, // Unichain - 130
+  ChainId.APE, // Apechain - 13371
 
   // Sei Pacific-1 - 1329
   // ZetaChain - 7000
